a5edb2e822ab47b3776e8332cf50122b7a9f70da
sjoongh <az45687@naver.com>
2021-09-08 15:39:13 +0900

    ejs

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 //  set(키, 값)
 //  get(키)
 
+// etag를 사용하지 않음
+app.set("etag", false);
+
 //  환경 변수에 PORT 가 설정되어 있으면 그 값을 사용
 //  설정이 안되어 있으면 3000
 app.set('port', process.env.PORT || 3000);
@@ -24,43 +27,67 @@ app.use(logger("dev"));
 //  미들웨어 express.static 미들웨어 함수를 등록 
 app.use(express.static(__dirname + "/public"));
 
+// View엔진 설정
+app.set("view engine", "ejs"); // 뷰엔진으로 ejs 사용 선언
+app.set("views", __dirname + "/views"); // 템플릿의 위치
+
 // GET 메서드 요청의 처리
 // app.get(url, callback)
 app.get("/", (req, resp) => {
     // http 모듈의 응답 처리메서드
-    console.log("[GET] /");
-    resp.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-    resp.write("Express Welcomes You!");
-    resp.end();
+    // console.log("[GET] /");
+    // resp.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    // resp.write("Express Welcomes You!");
+    // resp.end();
+    resp.status(200)
+        .contentType("text/html;charset=utf-8")
+        .render("home");
 })
 
 app.get("/welcome", (req, resp) => {
     // express의 추가 응답 처리 메서드
     console.log("[GET] /welcome");
     resp.status(200)
-    .header("Content-Type", "text/html;charset=UTF-8")
+    .header("Content-Type", "text/html;charset=utf-8")
     .send("Welcome!");
 })
 
-// GET 요청 파라미터의 처리
+//  GET 요청 파라미터의 처리
 app.get("/request", (req, resp) => {
-    console.log("[GET] / request");
-    console.log(req.query); // req.query -> url 파라미터 객체
-    console.log("[QUERY] name:"+req.query.name);
+    console.log("[GET] /request");
+    console.log(req.query); //  req.query -> url 파라미터 객체
+    console.log("[QUERY] name:" + req.query.name);
 
     let paramName = req.query.name;
     if (paramName === undefined ||
-        paramName.length == 0) { // name 파라미터가 전달되지 않으면
-            resp.status(404)    // NotFound
+        paramName.length == 0) {    //  name 파라미터가 전달되지 않으면
+        resp.status(404)    //  Not Found
             .contentType("text/html;charset=utf-8")
             .send("Name 정보를 확인할 수 없어요.");
-        } 
-    else {
-            // 파라미터 정상 전달
-            resp.status(200) // 성공
-            .contestType("text/html;charset=utf-8")
-            .send("Name:"+paramName);
-        }
+    } else {
+        //  파라미터 정상 전달
+        resp.status(200)    //  성공
+            .contentType("text/html;charset=utf-8")
+            .send("Name:" + paramName);
+    }
+})
+
+// URL 파라미터 처리(Fancy URL, Pretty URL)
+// URL 경로의 일부로 데이터 전송 방식
+app.get("/urlparam/:name", (req, resp) => {
+    // url 파라미터는 params 객체로 얻어온다.
+    let userName = req.params.name;
+    resp.writeHead(200, { "Content-Type": "text/html;charset=UTF-8"})
+    resp.write("<h1>name:" + userName + "</h1>")
+    resp.write("<p>URL 파라미터를 전달 받았습니다.</p>")
+    resp.send();
+})
+
+// 뷰엔진 활용
+app.get("/render", (req, resp) => {
+    // 응답객체의 render 메소드 활용
+    resp.contentType("text/html;charset=utf-8")
+    .render("render");
 })
 //  서버 start
 http.createServer(app).listen(app.get("port"), () => {
